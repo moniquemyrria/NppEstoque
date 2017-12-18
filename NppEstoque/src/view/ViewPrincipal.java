296517a0092badb989a6f30443e14747b80c7a68
Monique Rocha <moniquermyrria@gmail.com>
2017-12-18 10:06:17 -0400

    - criando chamada de viewmenu em viewprincipal

--- NppEstoque/src/view/ViewPrincipal.java
+++ NppEstoque/src/view/ViewPrincipal.java
@@ -1,21 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
-/**
- *
- * @author MONIQUE ROCHA
- */
+import controller_dao.ConfiguracaoDao;
+import controller_unificados.MensagemValidacaoController;
+import model_bean.ConfiguracaoModel;
+import model_conexao.Conexao;
+
 public class ViewPrincipal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private void consultaConexao() {
+        ConfiguracaoModel configModel = new ConfiguracaoModel();
+        ConfiguracaoDao configDao = new ConfiguracaoDao();
+        MensagemValidacaoController msg = new MensagemValidacaoController();
+        configModel = configDao.consultarArquivoConexaoDB();
+
+        if (configModel == null) {
+            msg.mensagemErro("Arquivo de conexão com o Banco de Dados não localizado.\n"
+                    + "Verifique se o diretório: c:\\nppEstoque existe.\n"
+                    + "Caso não exista, o diretório deve ser criado manualmente.\nEm seguida realize as configuração de conexão com o banco de dados.\n\n"
+                    + "Botão de opção: CONFIGURAÇÕES");
+            ViewConfiguracao telaConfig = new ViewConfiguracao();
+            telaConfig.setVisible(true);
+
+        } else {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                Process proc = rt.exec("java -jar \"lib\\derbyrun.jar\" server start");
+                Conexao.getConexao();
+                ViewMenu telaMenu = new ViewMenu();
+                telaMenu.setVisible(true);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
     }
-    
+
+    public static void main(String[] args) {
+
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ViewProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ViewProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ViewProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ViewProduto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        ViewPrincipal principal = new ViewPrincipal();
+        principal.consultaConexao();
+
+    }
+
 }
